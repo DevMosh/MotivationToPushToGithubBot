93ee03025c073c85fc03b320ce370f707e02c706
DevMosh <49912219+DevMosh@users.noreply.github.com>
2023-10-30 17:32:17 +0300

    add class for parsing github calendar and stats contributions

---
+++ main.py
@@ -0,0 +1,52 @@
+import re
+from datetime import datetime, timedelta
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class ParsGitHub:
+    def __init__(self, username):
+        self.url = f'https://github.com/{username}?tab=overview&from=2023-10-01&to=2023-10-27'
+        # url = f'https://github.com/DevMosh?tab=overview&from=2023-10-01&to=2023-10-27'
+        self.response = requests.get(self.url)
+        self.soup = BeautifulSoup(self.response.text, 'html.parser')
+
+    # метод, который будет выводить
+    def respone_calendar(self, today=False, yesterday=False):
+        calendar = self.soup.find('table', class_="ContributionCalendar-grid js-calendar-graph-table").find('tbody')  # достаем таблицу
+        calendar = calendar.find_all('tr', style='height: 11px')  # достаем все линии из таблицы
+
+        dict_calendar_contributions = {}  # словарь в который будем добавлять дни
+        for date in calendar:  # проходимся по линиям
+            calendar_line = date.find_all('td', class_="ContributionCalendar-day")
+            for line in calendar_line:  # проходимся по дням в линии
+                dict_calendar_contributions[line['data-date']] = line.getText()[0:16]
+
+        sorted_dates = sorted(dict_calendar_contributions.items())  # делаем сортировку по дням, чтобы даты в словаре шли по возрстанию
+        dict_calendar_contributions = {}
+
+        for i in sorted_dates:
+            try:
+                contributions = int(''.join(re.findall(r'\b\d+\b', i[1])))
+            except ValueError:
+                contributions = 0
+
+            dict_calendar_contributions.update({i[0]: contributions})
+
+        dict_need_to_days = []
+        if today is True:
+            day = str(datetime.now())[0:10]
+            dict_need_to_days.append((day, dict_calendar_contributions[day]))
+        if yesterday is True:
+            day = str(datetime.now() - timedelta(days=1))[0:10]
+            dict_need_to_days.append((day, dict_calendar_contributions[day]))
+
+        if True not in [today, yesterday]:
+            return dict_calendar_contributions
+        else:
+            return dict_need_to_days
+
+
+pars = ParsGitHub('DevMosh')
+print(pars.respone_calendar(yesterday=True))
